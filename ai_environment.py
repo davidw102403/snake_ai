d4558b5d1ab7f3111ec594eba94f1e9ed1e61df1
davidw102403 <dwang102403@gmail.com>
2023-08-20 11:19:50 -0500

    make collision function public

--- ai_environment.py
+++ ai_environment.py
@@ -79,7 +79,7 @@ class SnakeGame:
         # check if game over
         reward = 0
         game_over = False
-        if self._is_collision() or self.frame_iteration > 100*len(self.snake):
+        if self.is_collision() or self.frame_iteration > 100*len(self.snake):
             game_over = True
             reward = -10
             return reward, game_over, self.score
@@ -144,7 +144,7 @@ class SnakeGame:
 
         self.head = Point(x, y)
 
-    def _is_collision(self, pt=None):
+    def is_collision(self, pt=None):
         if pt is None:
             pt = self.head
         # check if hit boundary
